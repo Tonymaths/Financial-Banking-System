3a8251acdb5426c6edc81d33a6b623efbc403fa2
Tonymaths <41554748+Tonymaths@users.noreply.github.com>
2018-08-16 01:45:51 +0100

    .

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,7 +157,7 @@ STATICFILES_DIRS=( os.path.join(PROJECT_ROOT, 'static'),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT= os.path.join(PROJECT_ROOT, "mediafiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
